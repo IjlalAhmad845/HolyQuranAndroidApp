806d907b181b514e1081869037e5ce6e5aefcd65
Ijlal Ahmad <ijlalahmad845@gmail.com>
2021-10-05 19:23:28 +0530

    Added Launcher Icon and Fixed screen rotation and dark mode issues

--- app/src/main/java/com/example/holyquran/Activities/MainActivity.java
+++ app/src/main/java/com/example/holyquran/Activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.holyquran.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -19,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
        // startActivity(new Intent(MainActivity.this,ResultActivity.class));
-
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     }
 
     public void chapterButton(View v){
